6f7a9a201faf810feac46fdccbab2affce7ad226
maniek698846 <mariuszsiennicki94@gmial.com>
2017-11-24 14:11:26 +0100

    Testowy Commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		if(i<10);
+		{
+			System.out.println("Mój pierwszy program");
+		}
+		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in);
+		String name;
+		System.out.println("Podaj swoje imie :");
+		name = scanner.nextLine();
+		System.out.println("Twoje imie to: "+name);
+		
+		
+	}
+
+}
